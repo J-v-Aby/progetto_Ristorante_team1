a57057c2d73c122f055c45247f3cac98fd134194
claudiocldr <claudiocaldarella13@gmail.com>
2023-03-29 11:14:38 +0200

    aggiunta classe bevande

--- Ristorante.java
+++ Ristorante.java
@@ -1,5 +1,18 @@
+import java.util.List;
+
 public class Ristorante {
     public static void main(String[] args) {
-
+        creazioneBevande();
     }
+
+    public static void creazioneBevande() {
+        Bevande acqua = new Bevande("Acqua", 1.00F);
+        Bevande acquaFrizzante = new Bevande("Acqua frizzante", 1.20F);
+        Bevande cocaCola = new Bevande("Coca Cola", 2.00F);
+        Bevande Sprite = new Bevande("Sprite", 2.00F);
+        Bevande fanta = new Bevande("Fanta", 2.00F);
+        System.out.println("Bevande:" + "\n" + acqua.getNome() + "\n" + acquaFrizzante.getNome() + "\n" + cocaCola.getNome() + "\n" + Sprite.getNome() + "\n" + fanta.getNome());
+    }
+
+
 }
