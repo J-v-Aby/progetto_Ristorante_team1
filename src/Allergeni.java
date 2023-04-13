d7d8d261f426a8584c0186e669a65505ee1b3b83
Andrea <spinolaandrea3@gmail.com>
2023-04-13 16:30:20 +0200

    Aggiunti allergeni primi piatti

---
+++ src/Allergeni.java
@@ -0,0 +1,19 @@
+public enum Allergeni {
+    UOVA("Uova"),
+    CROSTACEI("Crostacei"),
+    SEDANO("Sedano"),
+    GLUTINE("Glutine"),
+    ARACHIDI("Arachidi"),
+    MOLLUSCHI("Molluschi"),
+    FRUTTA_A_GUSCIO("Frutta a guscio"),
+    PESCE ("Pesce"),
+    LATTE("Latte"),
+    SENAPE("Senape"),
+    NICKEL("Nickel");
+
+String nomeAllergene;
+    Allergeni(String nomeAllergene) {
+        this.nomeAllergene = nomeAllergene;
+
+    }
+}
